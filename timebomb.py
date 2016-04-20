f94f52e4d24ea9463005694e8d54e3a95fd538e7
august <thomauugust@gmail.com>
2016-04-20 12:39:26 +1000

    fixed import error, haven't run it myself yet lol

--- timebomb.py
+++ timebomb.py
@@ -1,21 +1,23 @@
 def main():
     import subprocess
-    import sleep
+    from time import sleep
     import random
 
     subprocess.call(["shutdown","-s", "-f", "-t", "60"])
 
     print "You have 60 seconds until your computer shuts down"
-    sleep(1)
+    sleep(0.5)
     print "Guess a number between zero and a hundred"
     randNo = random.randint(0,100)
     def choice():
         guess = raw_input('> ')
         if guess > randNo:
             "You guessed too high."
+            sleep(0.5)
             choice()
         if guess < randNo:
             "You guessed too low."
+            sleep(0.5)
             choice()
         if guess == randNo:
             subprocess.call(["shutdown", "-a"])
